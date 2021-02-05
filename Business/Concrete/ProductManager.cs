c06132e0181601e422767434d727967783a08194
Fikret Gözütok <fikret_gztk@hotmail.com>
2021-02-06 00:49:07 +0300

    Add project files.

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        private IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public List<Product> GetAll()
+        {
+            //İş Kodları - If yapıları olur genellikle
+            //Örneğin yetkisi varsa
+            return _productDal.GetAll();
+        }
+
+        public List<Product> GetAllByCategoryID(int id)
+        {
+            return _productDal.GetAll(p=>p.CategoryId == id).ToList();
+        }
+
+        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        {
+            return _productDal.GetAll(p=>p.UnitPrice >= min && p.UnitPrice <= max);
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,73 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+            //Veriler bir veri tabanından gelmiş gibi simüle ediyoruz.
+            _products = new List<Product> { 
+                new Product{ProductId = 1, CategoryId = 1, ProductName = "Bardak", UnitPrice = 15, UnitsInStock = 15},
+                new Product{ProductId = 2, CategoryId = 1, ProductName = "Kamera", UnitPrice = 500, UnitsInStock = 3},
+                new Product{ProductId = 3, CategoryId = 2, ProductName = "Telefon", UnitPrice = 1500, UnitsInStock = 2},
+                new Product{ProductId = 4, CategoryId = 2, ProductName = "Klavye", UnitPrice = 150, UnitsInStock = 65},
+                new Product{ProductId = 5, CategoryId = 2, ProductName = "Fare", UnitPrice = 85, UnitsInStock = 1}
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+            //LINQ - Language Integrated Query
+            // (=>) Lambda işareti
+
+            Product productToDelete;
+
+            //Parametre olarak aldığımız product'un referansı değerleri aynı olsa bile listede olmayacağından dolayı direk remove demek işe yaramaz.
+
+            /*foreach (var p in _products)
+            {
+                if (p.ProductId == product.ProductId)
+                {
+                    productToDelete = p;
+                }
+            }*/
+
+            productToDelete = _products.SingleOrDefault(p=>p.ProductId == product.ProductId); //Bu işlem yukarıdaki foreach döngüsünün yaptığını tek başına yapar.
+
+            //SingleOrDefault iki tane sonuç bulursa hata verir. O yüzden Primary Key (ID gibi) aramalarında kullanılır.
+
+            _products.Remove(productToDelete);
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            return null;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            return _products;
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = _products.SingleOrDefault(p=>p.ProductId == product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.CategoryId = product.CategoryId;
+            productToUpdate.UnitPrice = product.UnitPrice;
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
